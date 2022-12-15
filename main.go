24bc14249f7346b106f4d3b6dc6f9ea46aca8b04
Tomer Shafir <tomer.shafir@aquasec.com>
2022-12-15 23:46:35 +0200

    Comments

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 var counter = 0
 
+// Handling function
 func hello(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -25,13 +26,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
 
-	s := "<html><body>Counter: {{.}}</body></html>\n"
+	s := "<html><body>POST requests that were made: {{.}}</body></html>\n"
 
 	tmpl := template.Must(template.New("test").Parse(s))
 
 	tmpl.Execute(os.Stdout, counter)
 }
 
+// Main function that sets the event handler and fires the server
 func main() {
 	http.HandleFunc("/", hello)
 
